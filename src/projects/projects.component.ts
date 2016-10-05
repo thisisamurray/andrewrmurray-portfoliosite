4691185c83a0ad9cfeadfb3dd7ed613f189ab391
A. Murray <andrew@cladwell.com>
2016-10-05 11:24:50 -0400

    refactored projects data into it's own service

---
+++ src/projects/projects.component.ts
@@ -0,0 +1,23 @@
+import {Component} from "@angular/core";
+import {ProjectsService} from "../shared/services/projects";
+
+@Component({
+  selector: "projects",
+  providers: [ProjectsService],
+  template: `
+    <h2>Projects:</h2>
+    <ul>
+      <li *ngFor="let key of projectKeys">
+        <p>{{ allProjects[key].name }}</p>
+        <p>{{ allProjects[key].desc }}</p>
+        <button>Learn More</button>
+      </li>
+    </ul>
+  `,
+})
+
+export class ProjectsComponent {
+  constructor(private projectsService: ProjectsService){};
+  public allProjects = this.projectsService.getProjects();
+  public projectKeys = Object.keys(this.allProjects);
+}
