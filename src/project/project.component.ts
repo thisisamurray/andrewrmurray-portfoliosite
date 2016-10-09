3e30d514c18c98887e048f1e33fc0a214994fbf1
A. Murray <andrew@cladwell.com>
2016-10-09 09:27:23 -0400

    Updates to individual project page

--- src/project/project.component.ts
+++ src/project/project.component.ts
@@ -6,25 +6,71 @@ import { ProjectsService } from "../shared/services/projects";
 @Component({
   selector: "project",
   providers: [ProjectsService],
+  styleUrls: ['../shared/scss/project.scss'],
   template: `
-    <button (click)="goBack()">Back</button>
-    <h2>Project: {{project.name}}</h2>
-    <img src="{{project.preview_image}}" />
-    <h1>{{project.name}}</h1>
-    <p>{{project.desc}}</p>
+    <div class="project-detail">
+      <div class="detail-bar">
+        <button class="small-button" (click)="goBack()"><i class="fa fa-chevron-left" aria-hidden="true"></i> Back</button>
+        <h2><span class="label">Project:</span> {{project.name}}</h2>
+      </div>
+      <div class="detail-content">
+        <p>{{project.desc}}</p>
+      </div>
+      <div class="detail-gallery">
+        <div class="gallery-navigator pure-menu-horizontal">
+          <ul class="navigator-buttons pure-menu-list">
+            <li [class.active]="i === currentImageIndex" *ngFor="let imgButton of project.images; let i = index" (click)="updateMainImage(i)" class="pure-menu-item">
+              {{i + 1}}
+            </li>
+          </ul>
+        </div>
+        <div class="gallery-container">
+          <button class="swiper left" (click)="swipe(SA.LEFT)"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
+          <div class="gallery_desc">
+            <h3>{{project.images[currentImageIndex].title}}</h3>
+            <p>{{project.images[currentImageIndex].desc}}</p>
+          </div>
+          <img class="gallery-image" src="{{project.images[currentImageIndex].url}}" (swipeleft)="swipe($event.type)" (swiperight)="swipe($event.type)" />
+          <button class="swiper right" (click)="swipe(SA.RIGHT)"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>
+        </div>
+      </div>
+    </div>
   `,
 })
 
 export class ProjectComponent implements OnInit {
+  public currentImageYOffset:number = 0;
+  public currentImageIndex:number = 0;
   public project:any
   constructor(private projectsService: ProjectsService, private route: ActivatedRoute, private location: Location){}
   ngOnInit() {
     this.route.params.forEach((params: Params) => {
       let uid = params['uid'];
-      this.project =this.projectsService.getProject(uid);
+      this.project = this.projectsService.getProject(uid);
     });
   }
   goBack(): void {
     this.location.back();
+  }
+  updateMainImage(index) {
+    this.currentImageIndex = index;
+  }
+  SA = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
+  PA = { DOWN: 'pandown', UP: 'panup' };
+  swipe(action = this.SA.RIGHT) {
+    if (action === this.SA.RIGHT) {
+      if (this.currentImageIndex === 0) {
+        this.currentImageIndex = this.project.images.length - 1;
+      } else {
+        this.currentImageIndex--;
+      }
+    }
+    if (action === this.SA.LEFT) {
+      if (this.currentImageIndex === this.project.images.length - 1) {
+        this.currentImageIndex = 0;
+      } else {
+        this.currentImageIndex++;
+      }
+    }
   }
 }
--- src/shared/services/projects.ts
+++ src/shared/services/projects.ts
@@ -5,24 +5,38 @@ export class ProjectsService {
   public allProjects = {
     cladwell_giftFinder: {
       name: "Men\'s Gift Finder",
+      time: "",
+      owner: "",
       ele: "Interactive Angular Web App to recommend Christmas Gifts.",
       desc: "Tasked with creating a interactive web application to educate and enable women to pair down their total amount of clothing, I aided the Cladwell executive team with outlining objectives, guiding UX, and managed project timelines while developing the front-end interface. Since this was a prototype of a new product concept, the code was developed in mind of restructuring and moving components. The development team turned to using React.js with this project first due it\'s modular nature.",
       preview_image: "./src/shared/img/projects/giftfinder0.png",
-      images: ["./shared/img/projects/giftfinder1.png", "./shared/img/projects/giftfinder2.png", "./shared/img/projects/giftfinder3.png"],
+      images: [
+        {title: "", desc: "", url: "./src/shared/img/projects/giftfinder1.png"},
+        {title: "", desc: "", url: "./src/shared/img/projects/giftfinder2.png"},
+        {title: "", desc: "", url: "./src/shared/img/projects/giftfinder3.png"}
+      ],
     },
     cladwell_capsules: {
       name: "Capsules Web App",
+      time: "",
+      owner: "",
       ele: "React Web App to help women manage their style and clothing.",
       desc: "Tasked with creating a interactive web application to educate and enable women to pair down their total amount of clothing, I aided the Cladwell executive team with outlining objectives, guiding UX, and managed project timelines while developing the front-end interface. Since this was a prototype of a new product concept, the code was developed in mind of restructuring and moving components. The development team turned to using React.js with this project first due it\'s modular nature.",
       preview_image: "./src/shared/img/projects/capsules0.png",
-      images: ["./src/shared/img/projects/capsules1.png"],
+      images: [
+        {title: "Prototyping", desc: "Color coded to show different phases of implementation, this low fidelity prototype was created to ensure all team members understood pending application updates and help propel the design team.", url: "./src/shared/img/projects/capsules1.png"}
+      ],
     },
     cladwell_macysIdeaLab: {
       name: "Macy\'s Idea Lab",
-      ele: "",
+      time: "",
+      owner: "",
+      ele: "Collaboration with Macys.com and Cladwell to intergrate products.",
       desc: "I was sent to work with a prototyping team at Macys.com in San Francisco to help them with developing a small web application using Cladwell\'s technology to promote Macy\'s merchandise. Helped onboard the Macys.com team with the specific project objectives and target audience, played a large role in defining the user experience, and facilitated communication between both companies to create an MVP within two weeks. *All designs are property of Macys.com",
       preview_image: "./src/shared/img/projects/macys0.jpg",
-      images: [],
+      images: [
+        {title: "", desc: "", url: null}
+      ],
     },
   }
   public hiddenProjects = {
